42fb6ba6798445a67cdaf45d5b2b9d6508f790de
Nick Ohman <nickohman@Nicks-MacBook-Pro.local>
2020-06-01 17:52:31 -0500

    mvp

--- index.js
+++ index.js
@@ -71,10 +71,28 @@ server.delete(`/api/users/:id`, (req, res) => {
     const urlId = req.params.id
     let filteredUsersArray = users.filter(user =>  user.id !== Number(urlId))
     if(!users){
-         return res.status(500).json({ errorMessage: "The user information could not be retrieved." })
+         return res.status(500).json({ errorMessage: "The user could not be removed." })
     }else if(!filteredUsersArray){
          return res.status(404).json({ message: "The user with the specified ID does not exist." })
     } else {
          return res.status(200).json(filteredUsersArray)
+    }
+})
+
+
+//handle put request to /api/users/:id
+server.put(`/api/users/:id`, (req, res) => {
+    const urlId = req.params.id
+    let singleUser = users.filter(user =>  user.id === Number(urlId))
+    const editedUser = req.body
+
+    if(!users){
+         return res.status(500).json({ errorMessage: "The user information could not be modified." })
+    } else if (!singleUser){
+         return res.status(404).json({ message: "The user with the specified ID does not exist." })
+    } else if (!editedUser.name || !editedUser.bio){
+         return res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+    } else {
+         return res.status(200).json(editedUser)
     }
 })
